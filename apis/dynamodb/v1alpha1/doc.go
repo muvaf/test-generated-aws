9f2ed66c1f22d1bca1028a4f34efdd7b411452d3
Muvaffak Onus <onus.muvaffak@gmail.com>
2020-10-23 13:44:49 +0300

    add generated dynamodb API types
    Signed-off-by: Muvaffak Onus <onus.muvaffak@gmail.com>

---
+++ apis/dynamodb/v1alpha1/doc.go
@@ -0,0 +1,5 @@
+// +kubebuilder:object:generate=true
+// Package v1alpha1 is the v1alpha1 version of the dynamodb.services.k8s.aws API.
+// +groupName=dynamodb.services.k8s.aws
+// +versionName=v1alpha1
+package v1alpha1
