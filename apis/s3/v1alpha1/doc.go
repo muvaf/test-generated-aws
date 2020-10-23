084f424c0316bd7555bfff728d93aec4b3fa9116
Muvaffak Onus <onus.muvaffak@gmail.com>
2020-10-23 13:47:27 +0300

    add generated s3 API types
    Signed-off-by: Muvaffak Onus <onus.muvaffak@gmail.com>

---
+++ apis/s3/v1alpha1/doc.go
@@ -0,0 +1,5 @@
+// +kubebuilder:object:generate=true
+// Package v1alpha1 is the v1alpha1 version of the s3.services.k8s.aws API.
+// +groupName=s3.services.k8s.aws
+// +versionName=v1alpha1
+package v1alpha1
